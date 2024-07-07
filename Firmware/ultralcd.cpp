bc69dfa7227f6f3345b882d1a763af8eb64edbaa
Joonas Onatsu <joonas@onatsu.net>
2024-07-07 05:07:19 +0300

    Config for my Remake MK3S

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -2876,7 +2876,8 @@ bool lcd_calibrate_z_end_stop_manual(bool only_z)
 calibrated:
     // Let the machine think the Z axis is a bit higher than it is, so it will not home into the bed
     // during the search for the induction points.
-	if ((PRINTER_TYPE == PRINTER_MK25) || (PRINTER_TYPE == PRINTER_MK2) || (PRINTER_TYPE == PRINTER_MK2_SNMM)) {
+    // https://github.com/bear-lab-3d/Prusa-Firmware/releases/tag/BEARCAL_3.10.1_f19c3a
+	if ((PRINTER_TYPE == PRINTER_MK25) || (PRINTER_TYPE == PRINTER_MK2) || (PRINTER_TYPE == PRINTER_MK2_SNMM) || (PRINTER_TYPE == PRINTER_MK3) || (PRINTER_TYPE == PRINTER_MK3S)) {
 		current_position[Z_AXIS] = Z_MAX_POS-3.f;
 	}
 	else {
