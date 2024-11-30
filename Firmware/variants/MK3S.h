f0ef7c5f73b19eb940944a93cda241a190899388
Joonas Onatsu <joonas@onatsu.net>
2024-11-30 23:00:07 +0200

    We have no Delta PSU, adjust settings

--- Firmware/variants/MK3S.h
+++ Firmware/variants/MK3S.h
@@ -29,7 +29,7 @@
 #define HAS_SECOND_SERIAL_PORT
 
 // PSU
-#define PSU_Delta                                 // uncomment if DeltaElectronics PSU installed
+//#define PSU_Delta                                 // uncomment if DeltaElectronics PSU installed
 
 
 // Uncomment the below for the E3D PT100 temperature sensor (with or without PT100 Amplifier)
@@ -354,7 +354,7 @@
 
 // Extruder cooling fans
 #define EXTRUDER_0_AUTO_FAN_PIN   8
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 45
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
 #define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
 #define EXTRUDER_ALTFAN_DETECT
 #define EXTRUDER_ALTFAN_SPEED_SILENT 255  // == full speed
